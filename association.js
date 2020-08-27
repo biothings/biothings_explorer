1c45db00c7f327d959fb2b0600bc5dae681baac2
jiwen xin <kevinxin@scripps.edu>
2020-08-26 21:23:11 -0700

    add associations endpoint

---
+++ association.js
@@ -0,0 +1,35 @@
+const meta_kg = require("@biothings-explorer/smartapi-kg");
+const kg = new meta_kg();
+META_KG_CONSTRUCTED = false
+
+module.exports = async (sub, obj, pred, api, source) => {
+    if (META_KG_CONSTRUCTED === false) {
+        await kg.constructMetaKG(true);
+        META_KG_CONSTRUCTED = true
+    }
+    let associations = [];
+    let filtered_res = kg.filter({
+        input_type: sub,
+        output_type: obj,
+        predicate: pred,
+        api_name: api,
+        source: source
+    })
+    filtered_res.map(op => {
+        associations.push({
+            subject: op.association.input_type,
+            object: op.association.output_type,
+            predicate: op.association.predicate,
+            provided_by: op.association.source,
+            api: {
+                name: op.association.api_name,
+                smartapi: {
+                    metadata: op.association.smartapi.meta.url,
+                    id: op.association.smartapi.id,
+                    ui: "https://smart-api.info/ui/" + op.association.smartapi.id
+                }
+            }
+        });
+    })
+    return associations;
+}
