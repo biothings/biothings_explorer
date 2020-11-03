3f98979e4387a1cfa2b8ece25dbe2b07253710ca
jiwen xin <kevinxin@scripps.edu>
2020-11-02 16:10:51 -0800

    fix undefined variable

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ const createServer = require("./server");
 
 app = createServer();
 
-let port = argv.port || 3000;
 
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
+app.listen(3000, () => console.log(`App listening at http://localhost:3000`))
