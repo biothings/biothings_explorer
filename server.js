c4e719d7ae119e99c7d95679e1514169bea097e0
jiwen xin <kevinxin@scripps.edu>
2020-11-06 11:00:31 -0800

    set rate limit for /query to be 200 requests per 15 mins

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const createServer = () => {
     app.use(helmet());
     const limiter = rateLimit({
         windowMs: 15 * 60 * 1000,
-        max: 100
+        max: 200
     });
     app.use("/query", limiter);
 
