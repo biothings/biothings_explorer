9ab307abed9ed0d26d463f10acfe7bb9054ac020
jiwen xin <kevinxin@scripps.edu>
2020-11-12 10:06:09 -0800

    add more test cases

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -1,31 +1,14 @@
 const app = require("../src/app");
 const request = require('supertest');
+const fs = require("fs");
+var path = require('path');
+
 
 describe("Testing endpoints", () => {
-    const query = {
-        "message": {
-            "query_graph": {
-                "edges": [
-                    {
-                        "id": "e10",
-                        "source_id": "n0",
-                        "target_id": "n1"
-                    }
-                ],
-                "nodes": [
-                    {
-                        "id": "n0",
-                        "type": "gene",
-                        "curie": "NCBIGene:1017"
-                    },
-                    {
-                        "id": "n1",
-                        "type": "chemical_substance"
-                    }
-                ]
-            }
-        }
-    }
+    const example_foler = path.resolve(__dirname, '../examples');
+    const gene2chemical_query = JSON.parse(fs.readFileSync(path.join(example_foler, 'query_chemicals_physically_interacts_with_genes.json')));
+    const disease2gene_query = JSON.parse(fs.readFileSync(path.join(example_foler, 'query_genes_relate_to_disease.json')));
+
     test("GET /", async () => {
         await request(app)
             .get("/")
@@ -58,10 +41,10 @@ describe("Testing endpoints", () => {
             })
     })
 
-    test("POST /query", async () => {
+    test("POST /query with gene2chemical query", async () => {
         await request(app)
             .post("/query")
-            .send(query)
+            .send(gene2chemical_query)
             .set('Accept', 'application/json')
             .expect(200)
             .expect('Content-Type', /json/)
@@ -72,10 +55,24 @@ describe("Testing endpoints", () => {
             })
     })
 
+    test("POST /query with disease2gene query", async () => {
+        await request(app)
+            .post("/query")
+            .send(disease2gene_query)
+            .set('Accept', 'application/json')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .then(response => {
+                expect(response.body).toHaveProperty("query_graph");
+                expect(response.body).toHaveProperty("knowledge_graph");
+                expect(response.body.knowledge_graph.edges[0].source_id).toEqual("MONDO:0016575");
+            })
+    })
+
     test("POST /smartapi/query", async () => {
         await request(app)
             .post("/smartapi/8f08d1446e0bb9c2b323713ce83e2bd3/query")
-            .send(query)
+            .send(gene2chemical_query)
             .set('Accept', 'application/json')
             .expect(200)
             .expect('Content-Type', /json/)
@@ -89,7 +86,7 @@ describe("Testing endpoints", () => {
     test("POST /source/query", async () => {
         await request(app)
             .post("/source/drugbank/query")
-            .send(query)
+            .send(gene2chemical_query)
             .set('Accept', 'application/json')
             .expect(200)
             .expect('Content-Type', /json/)
