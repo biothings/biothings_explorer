b532de15e7641104f4c15ab4c6fb810329192af5
jiwen xin <kevinxin@scripps.edu>
2020-11-12 09:36:21 -0800

    send error message if performance file not exits

--- src/routes/performance.js
+++ src/routes/performance.js
@@ -3,7 +3,14 @@ var path = require('path');
 class RoutePerformance {
     setRoutes(app) {
         app.get('/performance', function (req, res) {
-            res.sendFile(path.join(path.resolve(__dirname, '../..'), 'performance-test/report.html'));
+            const file_path = path.join(path.resolve(__dirname, '../..'), 'performance-test/report.html');
+            try {
+                if (fs.existsSync(file_path)) {
+                    res.sendFile(file_path)
+                }
+            } catch (err) {
+                res.end(JSON.stringify({ "error": "file not exists" }));
+            }
         });
     }
 }
