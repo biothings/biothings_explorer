44b296cff319f88617428e0c6206d5645314c56d
jiwen xin <kevinxin@scripps.edu>
2020-12-02 15:37:57 -0800

    add log as part of the outputs

--- src/controllers/QueryGraphHandler/index.js
+++ src/controllers/QueryGraphHandler/index.js
@@ -2,17 +2,21 @@ const BatchEdgeQueryHandler = require("./batch_edge_query");
 const QueryGraph = require("./query_graph")
 const KnowledgeGraph = require("./knowledge_graph");
 const QueryResults = require("./query_results");
+const InvalidQueryGraphError = require("../../utils/errors/invalid_query_graph_error");
 
 module.exports = class TRAPIQueryHandler {
     constructor() {
-
+        this.logs = [];
     }
 
     getResponse() {
         return {
-            query_graph: this.queryGraph,
-            knowledge_graph: this.knowledgeGraph.kg,
-            results: this.queryResults.getResults(),
+            message: {
+                query_graph: this.queryGraph,
+                knowledge_graph: this.knowledgeGraph.kg,
+                results: this.queryResults.getResults(),
+            },
+            logs: this.logs
         }
     }
 
@@ -35,14 +39,16 @@ module.exports = class TRAPIQueryHandler {
      */
     _processQueryGraph(queryGraph) {
         let queryGraphHandler = new QueryGraph(queryGraph);
-        return queryGraphHandler.createQueryPaths();
+        let res = queryGraphHandler.createQueryPaths();
+        this.logs = [...this.logs, ...queryGraphHandler.logs];
+        return res;
     }
 
     _createBatchEdgeQueryHandlers(queryPaths) {
         let handlers = {};
         for (const index in queryPaths) {
             handlers[index] = new BatchEdgeQueryHandler();
-            handlers[index].setEdges(queryPaths[0]);
+            handlers[index].setEdges(queryPaths[index]);
             handlers[index].subscribe(this.knowledgeGraph);
             handlers[index].subscribe(this.queryResults);
         };
@@ -55,13 +61,18 @@ module.exports = class TRAPIQueryHandler {
         try {
             queryPaths = this._processQueryGraph(this.queryGraph);
         } catch (err) {
-            throw 400;
+            if (err instanceof InvalidQueryGraphError) {
+                throw err;
+            } else {
+                throw new InvalidQueryGraphError();
+            }
         }
 
         const handlers = this._createBatchEdgeQueryHandlers(queryPaths);
         for (let i = 0; i < Object.keys(handlers).length; i++) {
             let handler = handlers[i];
             let res = await handler.query(handler.qEdges);
+            this.logs = [...this.logs, ...handler.logs];
             handler.notify(res);
         }
     }
