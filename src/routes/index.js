5406a1bd3ddc59eb214abebbe16d2466f609af9a
jiwen xin <kevinxin@scripps.edu>
2020-12-02 15:37:36 -0800

    add logging and error handling middleware to routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,8 @@ const routesQueryByAPI = require("./query_by_api");
 const routesQueryBySource = require("./query_by_source");
 const routesQuery = require("./query");
 const routesV1Query = require("./query_v1");
+const ErrorHandler = require("../middlewares/error");
+const LoggingHandler = require("../middlewares/logging");
 
 class Routes {
     setRoutes(app) {
@@ -16,7 +18,9 @@ class Routes {
         routesQueryByAPI.setRoutes(app);
         routesQueryBySource.setRoutes(app);
         routesQuery.setRoutes(app);
+        LoggingHandler.setRoutes(app);
         routesV1Query.setRoutes(app);
+        ErrorHandler.setRoutes(app);
     }
 }
 
