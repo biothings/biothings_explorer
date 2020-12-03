37029af6d7f7de07721aa77abc8600397c9f07f2
jiwen xin <kevinxin@scripps.edu>
2020-12-03 10:10:32 -0800

    remove message from logging

--- src/middlewares/logging.js
+++ src/middlewares/logging.js
@@ -25,7 +25,6 @@ class LoggingHandler {
             ],
             requestWhitelist: ['headers', 'body'],
             meta: true, // optional: control whether you want to log the meta data about the request (default to true)
-            msg: "HTTP {{req.ip}} {{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
             expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
             colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
             ignoreRoute: function (req, res) { return false; }, // optional: allows to skip some log messages based on request and/or response
