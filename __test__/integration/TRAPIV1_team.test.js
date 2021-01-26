cf5665bed213bdfb762e6f6a313bb9365bda7dd0
jiwen xin <kevinxin@scripps.edu>
2021-01-25 16:05:29 -0800

    test: add test for /team/{team_name}/query endpoint
    use query example from multiomics provider
    
    https://github.com/biothings/BioThings_Explorer_TRAPI/issues/8

--- __test__/integration/TRAPIV1_team.test.js
+++ __test__/integration/TRAPIV1_team.test.js
@@ -7,6 +7,9 @@ var path = require('path');
 describe("Testing /team/{team_name}/query endpoints", () => {
     const targeted_association_foler = path.resolve(__dirname, '../../examples/v1/textmining/targeted_association_kp');
     const chemical2gene_query = JSON.parse(fs.readFileSync(path.join(targeted_association_foler, 'query_chemical_to_gene.json')));
+    const clinical_risk_kp_foler = path.resolve(__dirname, '../../examples/v1/multiomics/clinical_risk_kp');
+    const drugexposure2disease_query = JSON.parse(fs.readFileSync(path.join(clinical_risk_kp_foler, 'query_drug_exposure_to_disease.json')));
+
 
     test("POST /v1/team/{team_name}/query with text mining query example", async () => {
         await request(app)
@@ -22,6 +25,22 @@ describe("Testing /team/{team_name}/query endpoints", () => {
                 expect(response.body.message.knowledge_graph).toHaveProperty("nodes");
                 expect(response.body.message.knowledge_graph).toHaveProperty("edges");
                 expect(response.body.message.knowledge_graph.nodes).toHaveProperty("PR:000008790");
+            })
+    })
+    test("POST /v1/team/{team_name}/query with multiomics query example", async () => {
+        await request(app)
+            .post("/v1/team/Multiomics Provider/query")
+            .send(drugexposure2disease_query)
+            .set('Accept', 'application/json')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .then((response) => {
+                expect(response.body).toHaveProperty("message");
+                expect(response.body.message).toHaveProperty("query_graph");
+                expect(response.body.message).toHaveProperty("knowledge_graph");
+                expect(response.body.message.knowledge_graph).toHaveProperty("nodes");
+                expect(response.body.message.knowledge_graph).toHaveProperty("edges");
+                expect(response.body.message.knowledge_graph.nodes).toHaveProperty("MONDO:0005015");
             })
     })
 })
