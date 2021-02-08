0aceaaf09df5069c489c414629aa31896b83863e
jiwen xin <kevinxin@scripps.edu>
2021-02-07 21:23:33 -0800

    test: :white_check_mark: fix failed test for reverse module

--- __test__/integration/reverse.test.js
+++ __test__/integration/reverse.test.js
@@ -1,33 +1,28 @@
-const Reverse = require("../../src/controllers/QueryGraphHandler/reverse");
+const reverse = require("../../src/controllers/QueryGraphHandler/reverse");
 
 describe("Test EdgeReverse class", () => {
     test("test reverse with correct predicate", () => {
-        const handler = new Reverse();
-        const res = handler.reverse('treats');
+        const res = reverse.reverse('treats');
         expect(res).toBe("treated_by");
     })
 
     test("test reverse with correct predicate if it contains underscore", () => {
-        const handler = new Reverse();
-        const res = handler.reverse('treated_by');
+        const res = reverse.reverse('treated_by');
         expect(res).toBe("treats");
     })
 
     test("test reverse with predicate having symmetric equal to true", () => {
-        const handler = new Reverse();
-        const res = handler.reverse('correlated_with');
+        const res = reverse.reverse('correlated_with');
         expect(res).toBe("correlated_with");
     })
 
     test("test predicate with no inverse property and symmetric not equal to true", () => {
-        const handler = new Reverse();
-        const res = handler.reverse('has_phenotype');
+        const res = reverse.reverse('has_phenotype');
         expect(res).toBeUndefined();
     })
 
     test("test predicate not exist in biolink model", () => {
-        const handler = new Reverse();
-        const res = handler.reverse('haha');
+        const res = reverse.reverse('haha');
         expect(res).toBeUndefined();
     })
 })
