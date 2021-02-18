c9cc95a814515f4d4ba3779b83c3b83b3054caab
jiwen xin <kevinxin@scripps.edu>
2021-02-18 07:51:21 -0800

    test: :white_check_mark: update test

--- __test__/integration/KnowledgeGraph.test.js
+++ __test__/integration/KnowledgeGraph.test.js
@@ -9,7 +9,11 @@ describe("Testing KnowledgeGraph Module", () => {
     const record = {
         "$edge_metadata": {
             trapi_qEdge_obj: edge1,
+            source: "DGIdb",
+            api_name: "BioThings DGIDB API"
         },
+        "publications": ['PMID:123', 'PMID:1234'],
+        "interactionType": "inhibitor",
         "$input": {
             original: "SYMBOL:CDK2",
             obj: {
@@ -55,6 +59,58 @@ describe("Testing KnowledgeGraph Module", () => {
             expect(res).toHaveProperty("name", "RILUZOLE");
             expect(res.attributes[0]).toHaveProperty("type", "biolink:id");
             expect(res.attributes[0]).toHaveProperty("value", ['CHEMBL.COMPOUND:CHEMBL744', 'PUBCHEM:1234', "name:RILUZOLE"])
+        })
+    })
+
+    describe("Testing _createAttributes function", () => {
+        test("test edge attribute provided_by and api are correctly found", () => {
+            const kg = new KnowledgeGraph();
+            const res = kg._createAttributes(record);
+            expect(res.length).toBeGreaterThanOrEqual(2);
+            expect(res[0]).toHaveProperty("name", "provided_by");
+            expect(res[0]).toHaveProperty("type", "biolink:provided_by");
+            expect(res[0]).toHaveProperty("value", "DGIdb");
+            expect(res[1]).toHaveProperty("name", "api");
+            expect(res[1]).toHaveProperty("type", "bts:api");
+            expect(res[1]).toHaveProperty("value", "BioThings DGIDB API");
+        })
+
+        test("test edge attribute other than provided_by and api are correctly found", () => {
+            const kg = new KnowledgeGraph();
+            const res = kg._createAttributes(record);
+            expect(res.length).toBeGreaterThan(2);
+            expect(res[2]).toHaveProperty("name", "publications");
+            expect(res[2]).toHaveProperty("type", "biolink:publications");
+            expect(res[2]).toHaveProperty("value", ['PMID:123', 'PMID:1234']);
+            expect(res[3]).toHaveProperty("name", "interactionType");
+            expect(res[3]).toHaveProperty("type", "bts:interactionType");
+            expect(res[3]).toHaveProperty("value", 'inhibitor');
+        })
+    })
+
+    describe("Testing _createEdge function", () => {
+        test("test edge attribute provided_by and api are correctly found", () => {
+            const kg = new KnowledgeGraph();
+            const res = kg._createAttributes(record);
+            expect(res.length).toBeGreaterThanOrEqual(2);
+            expect(res[0]).toHaveProperty("name", "provided_by");
+            expect(res[0]).toHaveProperty("type", "biolink:provided_by");
+            expect(res[0]).toHaveProperty("value", "DGIdb");
+            expect(res[1]).toHaveProperty("name", "api");
+            expect(res[1]).toHaveProperty("type", "bts:api");
+            expect(res[1]).toHaveProperty("value", "BioThings DGIDB API");
+        })
+
+        test("test edge attribute other than provided_by and api are correctly found", () => {
+            const kg = new KnowledgeGraph();
+            const res = kg._createAttributes(record);
+            expect(res.length).toBeGreaterThan(2);
+            expect(res[2]).toHaveProperty("name", "publications");
+            expect(res[2]).toHaveProperty("type", "biolink:publications");
+            expect(res[2]).toHaveProperty("value", ['PMID:123', 'PMID:1234']);
+            expect(res[3]).toHaveProperty("name", "interactionType");
+            expect(res[3]).toHaveProperty("type", "bts:interactionType");
+            expect(res[3]).toHaveProperty("value", 'inhibitor');
         })
     })
 
