00fe5aaa28b102b7a52ef069603890d84d4cc117
jiwen xin <kevinxin@scripps.edu>
2021-02-18 07:03:31 -0800

    test: :white_check_mark: fix test for KnowledgeGraph module

--- __test__/integration/KnowledgeGraph.test.js
+++ __test__/integration/KnowledgeGraph.test.js
@@ -6,55 +6,55 @@ describe("Testing KnowledgeGraph Module", () => {
     const gene_node1 = new QNode("n1", { category: "Gene", id: "NCBIGene:1017" });
     const chemical_node1 = new QNode("n3", { category: "ChemicalSubstance" });
     const edge1 = new QEdge("e01", { subject: gene_node1, object: chemical_node1 });
+    const record = {
+        "$edge_metadata": {
+            trapi_qEdge_obj: edge1,
+        },
+        "$input": {
+            original: "SYMBOL:CDK2",
+            obj: {
+                primaryID: 'NCBIGene:1017',
+                label: "CDK2",
+                dbIDs: {
+                    SYMBOL: "CDK2",
+                    NCBIGene: "1017"
+                },
+                curies: ['SYMBOL:CDK2', 'NCBIGene:1017']
+            }
+        },
+        "$output": {
+            original: "CHEMBL.COMPOUND:CHEMBL744",
+            obj: {
+                primaryID: 'CHEMBL.COMPOUND:CHEMBL744',
+                label: "RILUZOLE",
+                dbIDs: {
+                    "CHEMBL.COMPOUND": "CHEMBL744",
+                    "PUBCHEM": "1234",
+                    "name": "RILUZOLE"
+                },
+                curies: ['CHEMBL.COMPOUND:CHEMBL744', 'PUBCHEM:1234', "name:RILUZOLE"]
+            }
+        },
+    }
     describe("Testing _createInputNode function", () => {
         test("test when input with string, should output a hash of 40 characters", () => {
-            const record = {
-                "$reasoner_edge": edge1,
-                "$input": "SYMBOL:CDK2",
-                "id": "CHEMBL.COMPOUND:CHEMBL354634",
-                "$original_input": {
-                    "SYMBOL:CDK2": "NCBIGene:1017"
-                },
-                "$input_resolved_identifiers": {
-                    "NCBIGene:1017": {
-                        id: {
-                            label: "CDK2"
-                        }
-                    }
-                },
-                "label": "DRUG A"
-            }
             const kg = new KnowledgeGraph();
             const res = kg._createInputNode(record);
-            expect(res).toHaveProperty("NCBIGene:1017");
-            expect(res["NCBIGene:1017"]).toHaveProperty("name", "CDK2");
-            expect(res["NCBIGene:1017"]).toHaveProperty("category", "biolink:Gene");
-            expect(res["NCBIGene:1017"].attributes[0]).toHaveProperty("type", "biolink:id");
+            expect(res).toHaveProperty("category", "biolink:Gene");
+            expect(res).toHaveProperty("name", "CDK2");
+            expect(res.attributes[0]).toHaveProperty("type", "biolink:id");
+            expect(res.attributes[0]).toHaveProperty("value", ["SYMBOL:CDK2", "NCBIGene:1017"])
         })
     })
 
     describe("Testing _createOutputNode function", () => {
         test("test when input with string, should output a hash of 40 characters", () => {
-            const record = {
-                "$reasoner_edge": edge1,
-                "$input": "SYMBOL:CDK2",
-                "id": "CHEMBL.COMPOUND:CHEMBL354634",
-                "$original_input": {
-                    "SYMBOL:CDK2": "NCBIGene:1017"
-                },
-                "$input_resolved_identifiers": {
-                    "NCBIGene:1017": {
-                        id: {
-                            label: "CDK2"
-                        }
-                    }
-                },
-                "label": "DRUG A"
-            }
             const kg = new KnowledgeGraph();
             const res = kg._createOutputNode(record);
-            expect(res["CHEMBL.COMPOUND:CHEMBL354634"]).toHaveProperty("name", "DRUG A");
-            expect(res["CHEMBL.COMPOUND:CHEMBL354634"]).toHaveProperty("category", "biolink:ChemicalSubstance");
+            expect(res).toHaveProperty("category", "biolink:ChemicalSubstance");
+            expect(res).toHaveProperty("name", "RILUZOLE");
+            expect(res.attributes[0]).toHaveProperty("type", "biolink:id");
+            expect(res.attributes[0]).toHaveProperty("value", ['CHEMBL.COMPOUND:CHEMBL744', 'PUBCHEM:1234', "name:RILUZOLE"])
         })
     })
 
