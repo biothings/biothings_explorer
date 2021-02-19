41bf21ce7bda9218fb25e0ba19427350e6f59b75
jiwen xin <kevinxin@scripps.edu>
2021-02-18 20:20:43 -0800

    test: :white_check_mark: add test for / endpoint

---
+++ __test__/integration/controllers/routes/entry.test.js
@@ -0,0 +1,13 @@
+const app = require("../../../../src/app");
+const request = require('supertest');
+
+describe("Test entry point", () => {
+    test("query / should redirect to SmartAPI", async () => {
+        await request(app)
+            .get("/")
+            .expect(302)
+            .then((response) => {
+                expect(response.headers.location).toEqual("https://smart-api.info/ui/dc91716f44207d2e1287c727f281d339");
+            })
+    })
+})
