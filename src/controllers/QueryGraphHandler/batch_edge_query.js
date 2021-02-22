3f679d6ef0b0fe59e673dd8e366b0bb17e7a7b23
jiwen xin <kevinxin@scripps.edu>
2021-02-21 21:49:49 -0800

    refactor: :fire: remove comments

--- src/controllers/QueryGraphHandler/batch_edge_query.js
+++ src/controllers/QueryGraphHandler/batch_edge_query.js
@@ -75,20 +75,6 @@ module.exports = class BatchEdgeQueryHandler {
         nodeUpdate.update(processed_query_res);
         debug('update nodes completed')
         return processed_query_res;
-        // let cacheHandler = new CacheHandler(qEdges);
-        // let { cachedResults, nonCachedEdges } = await cacheHandler.categorizeEdges(qEdges);
-        // let nodeUpdate = new NodesUpdateHandler(nonCachedEdges);
-        // await nodeUpdate.setEquivalentIDs(nonCachedEdges);
-        // let edgeConverter = new QEdge2BTEEdgeHandler(nonCachedEdges, this.kg);
-        // let bteEdges = edgeConverter.convert(nonCachedEdges);
-        // this.logs = [...this.logs, ...edgeConverter.logs];
-        // let expanded_bteEdges = this._expandBTEEdges(bteEdges);
-        // let query_res = await this._queryBTEEdges(expanded_bteEdges);
-        // await cacheHandler.cacheEdges(query_res);
-        // query_res = [...query_res, ...cachedResults];
-        // let processed_query_res = await this._postQueryFilter(query_res);
-        // nodeUpdate.update(processed_query_res);
-        // return processed_query_res;
     }
 
     /**
