4f26dc2d823b68de0621f6350c833e722c8c8e68
jiwen xin <kevinxin@scripps.edu>
2021-02-21 20:41:45 -0800

    refactor: :recycle: update logging path

--- src/middlewares/logging.js
+++ src/middlewares/logging.js
@@ -7,7 +7,7 @@ class LoggingHandler {
     setRoutes(app) {
         const transport = new (winston.transports.DailyRotateFile)({
             filename: 'BioThings-Explorer-TRAPI-%DATE%.log',
-            dirname: (process.env.NODE_ENV !== "production") ? "." : "/var/log/bte",
+            dirname: process.env.LOG || './log',
             datePattern: 'YYYY-MM-DD',
             zippedArchive: true,
             maxSize: '20m',
