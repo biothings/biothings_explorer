44d14e585def749a5b10a63bc8acb4136ea308e9
jiwen xin <kevinxin@scripps.edu>
2021-03-12 10:53:31 -0800

    refactor: :recycle: fix broken code due to use new query_graph_handler dependency

--- src/routes/v1/query_v1.js
+++ src/routes/v1/query_v1.js
@@ -1,13 +1,14 @@
+const path = require("path");
 const TRAPIGraphHandler = require("@biothings-explorer/query_graph_handler");
-const swaggerValidation = require("../../middlewares/validate")
-
+const swaggerValidation = require("../../middlewares/validate");
+const smartAPIPath = path.resolve(__dirname, '../../../data/smartapi_specs.json');
 class V1RouteQuery {
     setRoutes(app) {
         app.post('/v1/query', swaggerValidation.validate, async (req, res, next) => {
             //logger.info("query /query endpoint")
             try {
                 const queryGraph = req.body.message.query_graph;
-                const handler = new TRAPIGraphHandler();
+                const handler = new TRAPIGraphHandler.TRAPIQueryHandler({}, smartAPIPath);
                 handler.setQueryGraph(queryGraph);
                 await handler.query();
                 res.setHeader('Content-Type', 'application/json');
