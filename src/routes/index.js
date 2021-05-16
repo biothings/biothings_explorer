7c7b8e1fed0f195c8ab5854b8a49d0cf9b71ae96
jiwen xin <kevinxin@scripps.edu>
2021-05-15 17:16:05 -0700

    feat: :sparkles: add metakg routes to index

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,7 @@ const routesV1QueryByAPI = require("./v1/query_v1_by_api");
 const routesV1QueryByTeam = require("./v1/query_v1_by_team");
 const routesQueryTest = require("./v1/query_test");
 const routesV1Query = require("./v1/query_v1");
+const routesV1MetaKG = require("./v1/meta_knowledge_graph_v1")
 const ErrorHandler = require("../middlewares/error");
 const LoggingHandler = require("../middlewares/logging");
 
@@ -15,6 +16,7 @@ class Routes {
     setRoutes(app) {
         routesMetaKG.setRoutes(app);
         routesV1Predicates.setRoutes(app);
+        routesV1MetaKG.setRoutes(app);
         routesV1PredicatesByAPI.setRoutes(app);
         routesV1PredicatesByTeam.setRoutes(app);
         routesFrontPage.setRoutes(app);
