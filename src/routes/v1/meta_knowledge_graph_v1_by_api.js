85ff733bbaefec91c55d36b9bf62bc4bc16259de
Chunlei Wu <anewgene@yahoo.com>
2021-05-22 14:52:50 -0700

    fix: :bug: fix meta_kg endpoint for a given API

--- src/routes/v1/meta_knowledge_graph_v1_by_api.js
+++ src/routes/v1/meta_knowledge_graph_v1_by_api.js
@@ -4,7 +4,7 @@ class RouteMetaKGByAPI {
     setRoutes(app) {
         app.get('/v1/smartapi/:smartapiID/meta_knowledge_graph', async (req, res, next) => {
             try {
-                const metaKGHandler = new handler(undefined, req.params.smartapiID);
+                const metaKGHandler = new handler(req.params.smartapiID);
                 const kg = await metaKGHandler.getKG();
                 res.setHeader('Content-Type', 'application/json');
                 res.end(JSON.stringify(kg));
