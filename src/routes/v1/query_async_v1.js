d1518d75bb2a297703a7560099c14f6ddc91e85b
sengineer0 <sengineer020@gmail.com>
2021-08-04 19:34:51 +0300

    feat: added webhook url on the query_async endpoint

--- src/routes/v1/query_async_v1.js
+++ src/routes/v1/query_async_v1.js
@@ -1,5 +1,6 @@
 const Queue = require('bull');
 const path = require("path");
+const axios = require('axios')
 const config = require("./config");
 const TRAPIGraphHandler = require("@biothings-explorer/query_graph_handler");
 const swaggerValidation = require("../../middlewares/validate");
@@ -13,15 +14,27 @@ queryQueue.on('global:completed', (jobId, result) => {
     console.log(`Job completed with result ${result}`);
 });
 
-async function jobToBeDone(queryGraph, caching){
+async function jobToBeDone(queryGraph, caching, webhook_url){
     const handler = new TRAPIGraphHandler.TRAPIQueryHandler({ apiNames: config.API_LIST, caching: caching }, smartAPIPath);
     handler.setQueryGraph(queryGraph);
     await handler.query();
-    return handler.getResponse();
+    const response = handler.getResponse();
+    if(webhook_url){
+        try{
+            const res = await axios.post(webhook_url, JSON.stringify(response), {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        }catch (e){
+            console.error(e);
+        }
+    }
+    return response;
 }
 
 queryQueue.process(async (job) => {
-    return jobToBeDone(job.data.queryGraph, job.data.caching);
+    return jobToBeDone(job.data.queryGraph, job.data.caching, job.data.webhook_url);
 });
 
 class V1RouteAsyncQuery {
@@ -29,7 +42,8 @@ class V1RouteAsyncQuery {
         app.post('/v1/async_query', swaggerValidation.validate, async (req, res, next) => {
             try {
                 // add job to the queue
-                let job = await queryQueue.add({queryGraph: req.body.message.query_graph, caching: req.query.caching});
+                let job = await queryQueue.add({queryGraph: req.body.message.query_graph,
+                    caching: req.query.caching, webhook_url: req.query.webhook_url});
                 res.setHeader('Content-Type', 'application/json');
                 // return the job id so the user can check on it later
                 res.end(JSON.stringify({id: job.id}));
