87ee3c27068e1d30a1bb6589c9b26413e68dea2a
Colleen Xu <colleenhxu@gmail.com>
2021-08-13 11:41:58 -0700

    eric's changes here

--- src/routes/v1/query_v1_by_team.js
+++ src/routes/v1/query_v1_by_team.js
@@ -2,6 +2,7 @@ const TRAPIGraphHandler = require("@biothings-explorer/query_graph_handler");
 const swaggerValidation = require("../../middlewares/validate")
 const path = require("path");
 const smartAPIPath = path.resolve(__dirname, '../../../data/smartapi_specs.json');
+const predicatesPath = path.resolve(__dirname, '../../../data/predicates.json');
 const utils = require("../../utils/common");
 
 class RouteQueryV1ByTeam {
@@ -18,7 +19,7 @@ class RouteQueryV1ByTeam {
                         enableIDResolution
                     },
                     smartAPIPath,
-                    undefined,
+                    predicatesPath,
                     false
                 );
                 handler.setQueryGraph(queryGraph);
