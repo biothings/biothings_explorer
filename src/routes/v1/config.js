845dd2518260636509dc7b9ba196b1600363be9c
Colleen Xu <colleenhxu@gmail.com>
2021-08-23 16:01:04 -0700

    chore: reorganize API list to make it easier to manually adjust what APIs BTE ingests

--- src/routes/v1/config.js
+++ src/routes/v1/config.js
@@ -1,4 +1,21 @@
 exports.API_LIST = [
+    // external APIs annotated with SmartAPI x-bte
+    {
+        id: 'd22b657426375a5295e7da8a303b9893',
+        name: 'BioLink API'
+    },
+    {
+        id: '43af91b3d7cae43591083bff9d75c6dd',
+        name: 'EBI Proteins API'
+    },
+    {
+        id: '1ad2cba40cb25cd70d00aa8fba9cfaf3',
+        name: 'LINCS Data Portal API'
+    },
+    {
+        id: 'dca415f2d792976af9d642b7e73f7a41',
+        name: 'LitVar API'
+    },
     {
         id: '1f277e1563fcfd124bfae2cc3c4bcdec',
         name: 'QuickGO API'
@@ -7,61 +24,10 @@ exports.API_LIST = [
         id: '1c056ffc7ed0dd1229e71c4752239465',
         name: 'Ontology Lookup Service API'
     },
+    // "pending" BioThings APIs
     {
-        id: 'dca415f2d792976af9d642b7e73f7a41',
-        name: 'LitVar API'
-    },
-    {
-        id: 'a5b0ec6bfde5008984d4b6cde402d61f',
-        name: 'Human Phenotype Ontology API'
-    },
-    {
-        id: 'ec6d76016ef40f284359d17fbf78df20',
-        name: 'UBERON Ontology API'
-    },
-    {
-        id: '671b45c0301c8624abbd26ae78449ca2',
-        name: 'MyDisease.info API'
-    },
-    {
-        id: '09c8782d9f4027712e65b95424adba79',
-        name: 'MyVariant.info API'
-    },
-    {
-        id: '59dce17363dce279d389100834e43648',
-        name: 'MyGene.info API'
-    },
-    {
-        id: 'ed0ee52921c7cbce24033ffd1369922e',
-        name: 'SEMMED Disease API'
-    },
-    {
-        id: '8f08d1446e0bb9c2b323713ce83e2bd3',
-        name: 'MyChem.info API'
-    },
-    {
-        id: '0c9f1154a1986f1774057af4c1caa5b2',
-        name: 'SEMMED Anatomy API'
-    },
-    {
-        id: '81955d376a10505c1c69cd06dbda3047',
-        name: 'SEMMED Gene API'
-    },
-    {
-        id: '2dffb89df7f970b6a07e816e255d33ec',
-        name: 'SEMMED Phenotype API'
-    },
-    {
-        id: '77ed27f111262d0289ed4f4071faa619',
-        name: 'MGIgene2phenotype API'
-    },
-    {
-        id: '5a7d625d50fc518d33db48cf39ce9b30',
-        name: 'SEMMED Biological Process API'
-    },
-    {
-        id: '7c07eca4ef5ceb532d06c0180e86aedd',
-        name: 'SEMMED Chemical API'
+        id: 'e3edd325c76f2992a111b43a907a4870',
+        name: 'BioThings DGIdb API'
     },
     {
         id: 'a7f784626a426d054885a5f33f17d3f8',
@@ -76,60 +42,97 @@ exports.API_LIST = [
         name: 'Gene Ontology Biological Process API'
     },
     {
-        id: 'd86a24f6027ffe778f84ba10a7a1861a',
-        name: 'Clinical Risk KP API'
+        id: 'f339b28426e7bf72028f60feefcd7465',
+        name: 'Gene Ontology Cellular Component API'
     },
     {
         id: '34bad236d77bea0a0ee6c6cba5be54a6',
         name: 'Gene Ontology Molecular Activity API'
     },
     {
-        id: 'f339b28426e7bf72028f60feefcd7465',
-        name: 'Gene Ontology Cellular Component API'
+        id: 'a5b0ec6bfde5008984d4b6cde402d61f',
+        name: 'Human Phenotype Ontology API'
     },
     {
-        id: 'e3edd325c76f2992a111b43a907a4870',
-        name: 'BioThings DGIdb API'
+        id: '77ed27f111262d0289ed4f4071faa619',
+        name: 'MGIgene2phenotype API'
     },
     {
-        id: '978fe380a147a8641caf72320862697b',
-        name: 'Text Mining Targeted Association API'
+        id: 'ec6d76016ef40f284359d17fbf78df20',
+        name: 'UBERON Ontology API'
+    },
+    // Core BioThings APIs
+    {
+        id: '8f08d1446e0bb9c2b323713ce83e2bd3',
+        name: 'MyChem.info API'
+    },
+    {
+        id: '671b45c0301c8624abbd26ae78449ca2',
+        name: 'MyDisease.info API'
+    },
+    {
+        id: '59dce17363dce279d389100834e43648',
+        name: 'MyGene.info API'
+    },
+    {
+        id: '09c8782d9f4027712e65b95424adba79',
+        name: 'MyVariant.info API'
+    },
+    // SEMMED BioThings APIs
+    {
+        id: '0c9f1154a1986f1774057af4c1caa5b2',
+        name: 'SEMMED Anatomy API'
+    },
+    {
+        id: '5a7d625d50fc518d33db48cf39ce9b30',
+        name: 'SEMMED Biological Process API'
+    },
+    {
+        id: '7c07eca4ef5ceb532d06c0180e86aedd',
+        name: 'SEMMED Chemical API'
+    },
+    {
+        id: 'ed0ee52921c7cbce24033ffd1369922e',
+        name: 'SEMMED Disease API'
+    },
+    {
+        id: '81955d376a10505c1c69cd06dbda3047',
+        name: 'SEMMED Gene API'
+    },
+    {
+        id: '2dffb89df7f970b6a07e816e255d33ec',
+        name: 'SEMMED Phenotype API'
+    },
+    // Multiomics Provider collab
+    {
+        id: 'd86a24f6027ffe778f84ba10a7a1861a',
+        name: 'Clinical Risk KP API'
     },
     {
         id: '02af7d098ab304e80d6f4806c3527027',
         name: 'Multiomics Wellness KP API'
     },
+    // Text Mining Provider collab
     {
-        id: 'd22b657426375a5295e7da8a303b9893',
-        name: 'BioLink API'
+        id: '978fe380a147a8641caf72320862697b',
+        name: 'Text Mining Targeted Association API'
     },
-    {
-        id: '1ad2cba40cb25cd70d00aa8fba9cfaf3',
-        name: 'LINCS Data Portal API'
-    },
-    {
-        id: '43af91b3d7cae43591083bff9d75c6dd',
-        name: 'EBI Proteins API'
-    },
-    {
-        id: 'bee0cc86f86d88b83f613b674e2bd92e',
-        name: 'Automat IntAct (trapi v-1.1.2)'
-    },
+    // Automat APIs
     {
         id: '0a93684206ec6a13812ff2867a445c4e',
         name: 'Automat Cord19 (trapi v-1.1.2)'
+    },
+    {
+        id: '1fd2f4cc6b3b6b1f7cda594b00607270',
+        name: 'Automat Foodb (trapi v-1.1.2)'
     },
     {
         id: 'c8b1619535bd598406049f4dc51e1702',
         name: 'Automat Gtopdb (trapi v-1.1.2)'
     },
     {
-        id: '994a667dafe2e2c1d42a5390a6fba9aa',
-        name: 'Automat Uberongraph (trapi v-1.1.2)'
-    },
-    {
-        id: '2c9fff2f09c71302659faeb515bbb2b8',
-        name: 'Automat Human GOA (trapi v-1.1.2)'
+        id: '3a8ad755bbd6f4fdfc0a18c8020d6d58',
+        name: 'Automat Hetio (trapi v-1.1.2)'
     },
     {
         id: '255ea57924924b721919b9ee17a07894',
@@ -140,8 +143,12 @@ exports.API_LIST = [
         name: 'Automat HMDB (trapi v-1.1.2)'
     },
     {
-        id: '3a8ad755bbd6f4fdfc0a18c8020d6d58',
-        name: 'Automat Hetio (trapi v-1.1.2)'
+        id: '2c9fff2f09c71302659faeb515bbb2b8',
+        name: 'Automat Human GOA (trapi v-1.1.2)'
+    },
+    {
+        id: 'bee0cc86f86d88b83f613b674e2bd92e',
+        name: 'Automat IntAct (trapi v-1.1.2)'
     },
     {
         id: 'c3e55e3a28cf14e147b55e6e09b32b9b',
@@ -152,8 +159,8 @@ exports.API_LIST = [
         name: 'Automat Pharos (trapi v-1.1.2)'
     },
     {
-        id: '1fd2f4cc6b3b6b1f7cda594b00607270',
-        name: 'Automat Foodb (trapi v-1.1.2)'
+        id: '994a667dafe2e2c1d42a5390a6fba9aa',
+        name: 'Automat Uberongraph (trapi v-1.1.2)'
     },
     // other Automat APIs we could try out
     // ask: is Automat CTD still up / valid?
@@ -179,20 +186,21 @@ exports.API_LIST = [
     //     id: 'bd612e18f86d9097b02c0d83344b46b7',
     //     name: 'Automat CTD (trapi v-1.1.2)'
     // },
+    // Clinical-data-based APIs from other Translator teams
     {
-        id: '65292eac9a88e3a895be21f19b554767',
-        name: 'ICEES COVID-19 Instance API'
+        id: '70117385218edc9bc01633829011dfcf',
+        name: 'Columbia Open Health Data (COHD)'
     },
     {
         id: '0864c0912390d0876c3c34a00acb5c3b',
         name: 'ICEES Asthma Instance API'
     },
     {
-        id: '9dd890397a7b8d98fbe247d56cac2b8f',
-        name: 'ICEES DILI Instance API'
+        id: '65292eac9a88e3a895be21f19b554767',
+        name: 'ICEES COVID-19 Instance API'
     },
     {
-        id: '70117385218edc9bc01633829011dfcf',
-        name: 'Columbia Open Health Data (COHD)'
+        id: '9dd890397a7b8d98fbe247d56cac2b8f',
+        name: 'ICEES DILI Instance API'
     },
 ];
