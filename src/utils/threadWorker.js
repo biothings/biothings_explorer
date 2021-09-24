134a41487b6dcf9b82a3eb0baa46c048a0e30b22
tokebe <43009413+tokebe@users.noreply.github.com>
2021-09-24 15:23:40 -0400

    feat: terminate unresponsive requests

--- src/utils/threadWorker.js
+++ src/utils/threadWorker.js
@@ -2,16 +2,21 @@ const { Worker } = require("worker_threads");
 const path = require("path");
 
 module.exports = function runWorker(task) {
-    return new Promise((resolve, reject) => {
-        const worker = new Worker(path.resolve(__dirname, "../server.js"), {
-            workerData: { req: task.req, route: task.route },
-        });
-        worker.on("message", resolve);
-        worker.on("error", reject);
-        worker.on("exit", code => {
-            if (code !== 0) {
-                reject(new Error(`Worker exited with code ${code}`));
-            }
-        });
+  return new Promise((resolve, reject) => {
+    const worker = new Worker(path.resolve(__dirname, "../server.js"), {
+      workerData: { req: task.req, route: task.route },
     });
+    worker.on("message", resolve);
+    worker.on("error", reject);
+    worker.on("exit", code => {
+      if (code !== 0) {
+        reject(new Error(`Worker exited with code ${code}`));
+      }
+    });
+    const timeout = parseInt(process.env.REQUEST_TIMEOUT || 300000);
+    setTimeout(() => {
+      worker.terminate();
+      reject(new Error(`Request timed out (exceeded time limit of ${timeout}ms)`));
+    }, timeout);
+  });
 };
