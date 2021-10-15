d2ae7fa86f519e0c61dc9659bceab4543ca850ce
tokebe <43009413+tokebe@users.noreply.github.com>
2021-10-15 14:32:49 -0400

    fix: don't set up cron in worker threads

--- src/server.js
+++ src/server.js
@@ -4,12 +4,11 @@ const { threadHandlers } = require("./routes/index");
 const { workerData, isMainThread, parentPort, Worker, threadId } = require("worker_threads");
 const debug = require("debug")(`bte:biothings-explorer-trapi:worker${threadId}`);
 
-const PORT = Number.parseInt(process.env.PORT) || 3000;
-
-cron();
 
 async function main() {
   if (isMainThread) {
+    const PORT = Number.parseInt(process.env.PORT) || 3000;
+    cron();
     app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
   } else {
     debug(`Worker thread ${threadId} beginning task.`);
