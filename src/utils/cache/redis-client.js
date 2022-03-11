8d88d6e1a66988984b16bcc6097eb1a517fe5dfd
tokebe <43009413+tokebe@users.noreply.github.com>
2022-03-11 12:18:25 -0500

    fix: redis timeout

--- src/utils/cache/redis-client.js
+++ src/utils/cache/redis-client.js
@@ -1,5 +1,6 @@
 const redis = require("redis");
-const { checkServerIdentity } = require("tls");
+// const { checkServerIdentity } = require("tls");
+// const redisLock = require('redis-lock');
 const { promisify } = require("util");
 
 let client;
@@ -16,13 +17,40 @@ if (enableRedis === true) {
     client = redis.createClient(details);
 }
 
-const redisClient = (enableRedis === true) ? {
+const timeoutFunc = (func, timeoutms=0) => {
+    return (...args) => {
+      return new Promise(async (resolve, reject) => {
+        const timeout = timeoutms
+          ? timeoutms
+          : parseInt(process.env.REDIS_TIMEOUT || 30000);
+        let done = false;
+        setTimeout(() => {
+          if (!done) {
+            reject(new Error(`redis call timed out, args: ${JSON.stringify(...args)}`));
+          }
+        }, timeout);
+        const returnValue = await func(...args);
+        done = true;
+        resolve(returnValue);
+      });
+    };
+  };
+
+const redisClient =
+enableRedis === true
+? {
     ...client,
-    getAsync: promisify(client.get).bind(client),
-    setAsync: promisify(client.set).bind(client),
-    hmsetAsync: promisify(client.hmset).bind(client),
-    keysAsync: promisify(client.keys).bind(client),
-    existsAsync: promisify(client.exists).bind(client)
-} : {};
+    getAsync: timeoutFunc(promisify(client.get).bind(client)),
+    setAsync: timeoutFunc(promisify(client.set).bind(client)),
+    hsetAsync: timeoutFunc(promisify(client.hset).bind(client)),
+    hgetallAsync: timeoutFunc(promisify(client.hgetall).bind(client)),
+    expireAsync: timeoutFunc(promisify(client.expire).bind(client)),
+    delAsync: timeoutFunc(promisify(client.del).bind(client)),
+    // lock: timeoutFunc(promisify(redisLock(client)), 20 * 60 * 1000),
+    hmsetAsync: timeoutFunc(promisify(client.hmset).bind(client)),
+    keysAsync: timeoutFunc(promisify(client.keys).bind(client)),
+    existsAsync: timeoutFunc(promisify(client.exists).bind(client)),
+    }
+: {};
 
 module.exports = redisClient;
