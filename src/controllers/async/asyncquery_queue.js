c98cff93fb88e7df63fa637ab4de3c3ad2d186d6
tokebe <43009413+tokebe@users.noreply.github.com>
2022-06-01 12:55:38 -0400

    Revert "fix: redis cluster support (hash tag prefix)"
    This reverts commit 0c887883724ac570bd064fe91532301cfc3c4fcd.

--- src/controllers/async/asyncquery_queue.js
+++ src/controllers/async/asyncquery_queue.js
@@ -5,63 +5,49 @@ const redisClient = require('@biothings-explorer/query_graph_handler').redisClie
 exports.getQueryQueue = name => {
     let queryQueue = null;
     if (Object.keys(redisClient).length !== 0 && !process.env.INTERNAL_DISABLE_REDIS) {
-        let details = {
-            redis: {
-                port: process.env.REDIS_PORT,
-                host: process.env.REDIS_HOST,
-            },
-            prefix: `{BTE:bull:${name}}`
-        };
+        let details = { port: process.env.REDIS_PORT, host: process.env.REDIS_HOST };
         if (process.env.REDIS_PASSWORD) {
             details.password = process.env.REDIS_PASSWORD;
         }
         if (process.env.REDIS_TLS_ENABLED) {
             details.tls = { checkServerIdentity: () => undefined }
         }
-        queryQueue = new Queue(
-          name,
-          process.env.REDIS_HOST
-            ? details
-            : "redis://127.0.0.1:6379",
-          {
+        queryQueue = new Queue(name, process.env.REDIS_HOST ? { redis: details } : "redis://127.0.0.1:6379", {
             defaultJobOptions: {
-              timeout: process.env.JOB_TIMEOUT,
-              removeOnFail: true,
+                timeout: process.env.JOB_TIMEOUT,
+                removeOnFail: true,
             },
             settings: {
-              maxStalledCount: 1,
-              //lockDuration: 300000
-              lockDuration: 3600000, // 60min
+                maxStalledCount: 1,
+                //lockDuration: 300000
+                lockDuration: 3600000, // 60min
             },
-          },
-        )
-          .on("error", function (error) {
+        }).on("error", function (error) {
             console.log("err", error);
-          })
-          .on("failed", async function (job, error) {
+        }).on("failed", async function (job, error) {
             console.log(`Async job ${job.id} failed with error ${error.message}`);
             console.trace(error);
             if (job.data.callback_url) {
-              try {
-                await axios({
-                  method: "post",
-                  url: job.data.callback_url,
-                  data: {
-                    message: {
-                      query_graph: job.data.queryGraph,
-                      knowledge_graph: { nodes: {}, edges: {} },
-                      results: [],
-                    },
-                    status: 500,
-                    description: error.toString(),
-                    trace: process.env.NODE_ENV === "production" ? undefined : error.stack,
-                  },
-                });
-              } catch (error) {
-                console.log(`Callback failed with error ${error.message}`);
-              }
+                try {
+                    await axios({
+                        method: 'post',
+                        url: job.data.callback_url,
+                        data: {
+                            message: {
+                                query_graph: job.data.queryGraph,
+                                knowledge_graph: { nodes: {}, edges: {} },
+                                results: []
+                            },
+                            status: 500,
+                            description: error.toString(),
+                            trace: process.env.NODE_ENV === 'production' ? undefined : error.stack
+                        }
+                    });
+                } catch (error) {
+                    console.log(`Callback failed with error ${error.message}`);
+                }
             }
-          });
+        });
     }
     return queryQueue;
 };
