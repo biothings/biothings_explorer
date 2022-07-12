aa0d0fb374385cdde8bb53bbc1bc42bc105fd763
tokebe <43009413+tokebe@users.noreply.github.com>
2022-07-12 15:58:25 -0400

    fix: format/indent, 405 for wrong operation

--- src/routes/frontpage.js
+++ src/routes/frontpage.js
@@ -1,9 +1,14 @@
+const utils = require("../utils/common");
+
 class RouteFrontPage {
-    setRoutes(app) {
-        app.get('/', (req, res) => {
-            res.redirect("https://smart-api.info/ui/dc91716f44207d2e1287c727f281d339");
-        })
-    }
+  setRoutes(app) {
+    app
+      .route("/")
+      .get((req, res) => {
+        res.redirect("https://smart-api.info/ui/dc91716f44207d2e1287c727f281d339");
+      })
+      .all(utils.methodNotAllowed);
+  }
 }
 
 module.exports = new RouteFrontPage();
--- src/routes/v1/asyncquery_v1_by_api.js
+++ src/routes/v1/asyncquery_v1_by_api.js
@@ -1,33 +1,35 @@
 const path = require("path");
 const swaggerValidation = require("../../middlewares/validate");
-const { asyncquery } = require('../../controllers/async/asyncquery');
-const { getQueryQueue } = require('../../controllers/async/asyncquery_queue');
+const { asyncquery } = require("../../controllers/async/asyncquery");
+const { getQueryQueue } = require("../../controllers/async/asyncquery_queue");
+const utils = require("../../utils/common");
 
-queryQueue = getQueryQueue('bte_query_queue_by_api')
+queryQueue = getQueryQueue("bte_query_queue_by_api");
 
 if (queryQueue) {
-    queryQueue.process(path.resolve(__dirname, "../../controllers/async/processors/async_v1_by_api.js"));
+  queryQueue.process(path.resolve(__dirname, "../../controllers/async/processors/async_v1_by_api.js"));
 }
 
 class V1RouteAsyncQueryByAPI {
-    setRoutes(app) {
-        app.post('/v1/smartapi/:smartapi_id/asyncquery', swaggerValidation.validate, async (req, res, next) => {
-            queryQueue = getQueryQueue('bte_query_queue_by_api')
-            // Disabled the disabling of text-mining / multiomics provider APIs -- we're not sure why they were disabled in the first place...
-            // const enableIDResolution = (['5be0f321a829792e934545998b9c6afe', '978fe380a147a8641caf72320862697b'].includes(req.params.smartapi_id)) ? false : true;
-            let queueData = {
-                queryGraph: req.body.message.query_graph,
-                smartAPIID: req.params.smartapi_id,
-                workflow: req.body.workflow,
-                callback_url: req.body.callback_url || req.body['callback'],
-                options: { logLevel: req.body.log_level, ...req.query },
-                // enableIDResolution
-            }
-            await asyncquery(req, res, next, queueData, queryQueue)
-        });
-    }
+  setRoutes(app) {
+    app
+      .route("/v1/smartapi/:smartapi_id/asyncquery")
+      .post(swaggerValidation.validate, async (req, res, next) => {
+        queryQueue = getQueryQueue("bte_query_queue_by_api");
+        // Disabled the disabling of text-mining / multiomics provider APIs -- we're not sure why they were disabled in the first place...
+        // const enableIDResolution = (['5be0f321a829792e934545998b9c6afe', '978fe380a147a8641caf72320862697b'].includes(req.params.smartapi_id)) ? false : true;
+        let queueData = {
+          queryGraph: req.body.message.query_graph,
+          smartAPIID: req.params.smartapi_id,
+          workflow: req.body.workflow,
+          callback_url: req.body.callback_url || req.body["callback"],
+          options: { logLevel: req.body.log_level, ...req.query },
+          // enableIDResolution
+        };
+        await asyncquery(req, res, next, queueData, queryQueue);
+      })
+      .all(utils.methodNotAllowed);
+  }
 }
-
-
 
 module.exports = new V1RouteAsyncQueryByAPI();
