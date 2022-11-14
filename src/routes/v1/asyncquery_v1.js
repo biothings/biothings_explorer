49d25b0fc854fcb041933142523719400c0f747e
tokebe <43009413+tokebe@users.noreply.github.com>
2022-11-14 15:18:03 -0500

    fix: global (to instance) queryQueue management

--- src/routes/v1/asyncquery_v1.js
+++ src/routes/v1/asyncquery_v1.js
@@ -4,19 +4,15 @@ const { asyncquery } = require("../../controllers/async/asyncquery");
 const { getQueryQueue } = require("../../controllers/async/asyncquery_queue");
 const utils = require("../../utils/common");
 
-const queryQueue = getQueryQueue("bte_query_queue");
-
-if (queryQueue) {
-  queryQueue.process(path.resolve(__dirname, "../../controllers/async/processors/async_v1.js"));
-}
-
 class V1RouteAsyncQuery {
   setRoutes(app) {
     app
       .route("/v1/asyncquery")
       .post(swaggerValidation.validate, async (req, res, next) => {
-        // if I don't reinitialize this then the wrong queue will be used, not sure why this happens
-        // queryQueue = getQueryQueue("bte_query_queue");
+        if (!global.QueryQueue) {
+          global.QueryQueue = getQueryQueue("bte_query_queue");
+          global.QueryQueue.process(path.resolve(__dirname, "../../controllers/async/processors/async_v1.js"));
+        }
 
         let queueData = {
           queryGraph: req.body.message.query_graph,
@@ -24,7 +20,7 @@ class V1RouteAsyncQuery {
           callback_url: req.body.callback_url || req.body["callback"],
           options: { logLevel: req.body.log_level, submitter: req.body.submitter, ...req.query },
         };
-        await asyncquery(req, res, next, queueData, queryQueue);
+        await asyncquery(req, res, next, queueData, global.QueryQueue);
       })
       .all(utils.methodNotAllowed);
   }
