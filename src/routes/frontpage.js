ac03f6c817e60efb84dec3133db885b03b364214
Marco Cano <artofmarco@gmail.com>
2023-03-10 15:02:25 -0800

    feat: :sparkles: Add BTE landing page

--- src/routes/frontpage.js
+++ src/routes/frontpage.js
@@ -1,7 +1,7 @@
 class RouteFrontPage {
     setRoutes(app) {
         app.get('/', (req, res) => {
-            res.redirect("https://smart-api.info/ui/dc91716f44207d2e1287c727f281d339");
+            res.send(__dirname + '../web-app/dist/index.html')
         })
     }
 }
