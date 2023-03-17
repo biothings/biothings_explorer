ea1e9ecb654fa21cb99b29b5c1f6f6639344683d
Marco Cano <artofmarco@gmail.com>
2023-03-17 13:00:45 -0700

    fix: :hammer: add footer credits

--- src/routes/frontpage.js
+++ src/routes/frontpage.js
@@ -2,7 +2,7 @@ const utils = require("../utils/common");
 
 class RouteFrontPage {
     setRoutes(app) {
-        app.get('/:var(about|try-it)?', (req, res) => {
+        app.get('/', (req, res) => {
             res.send(__dirname + '../web-app/dist/index.html')
         })
         .all(utils.methodNotAllowed);
