5ff27c705a6dd5d0214e2738e70b7fdf97c4708e
tokebe <43009413+tokebe@users.noreply.github.com>
2023-03-30 17:03:55 -0400

    build: migrate workspace in

---
+++ scripts/get_rev.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+current_folder=`pwd`
+
+echo '# Generated from "npm run get_rev" on ' $(date)
+
+echo `git ls-remote --get-url` `git rev-parse --short HEAD` `git rev-parse HEAD`
+
+while read line || [ -n "$line" ];
+do
+    read -r url module_dir <<< $line
+    cd "$module_dir"
+    echo `git ls-remote --get-url` `git rev-parse --short HEAD` `git rev-parse HEAD`
+    cd $current_folder
+done < scripts/packages.txt
