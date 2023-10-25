e910552aa8d6c20cf4fef6696ffe78ba292bf51f
tokebe <43009413+tokebe@users.noreply.github.com>
2023-10-25 13:45:09 -0400

    fix: smartapi_sync handling

--- scripts/smartapi_sync.js
+++ scripts/smartapi_sync.js
@@ -1,2 +1,4 @@
-sync = require("../src/controllers/cron/update_local_smartapi.js");
+process.env.DEBUG="bte*"
+process.env.SYNC_AND_EXIT="true" 
+sync = require("../packages/bte-server/built/controllers/cron/update_local_smartapi.js");
 sync();
