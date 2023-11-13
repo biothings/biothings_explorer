ab04dffa9970805f63b94f32f84d4f136431d3a7
tokebe <43009413+tokebe@users.noreply.github.com>
2023-11-13 15:56:57 -0500

    test: slightly improved concurrency test

--- performance-test/20-creative.js
+++ performance-test/20-creative.js
@@ -2,6 +2,8 @@ const debug = require("debug")("concurrency-test");
 const axios = require("axios");
 const async = require("async");
 
+const CONCURRENCY = 20;
+
 const TEST_URL = "https://api.bte.ncats.io";
 const TEST_ENDPOINT = "/v1/asyncquery";
 
@@ -19,7 +21,9 @@ const IDS = [
 
 const start = performance.now();
 
-let results = Array(20).fill(0);
+let results = Array(CONCURRENCY).fill(0);
+
+console.log(`Beginning test of ${CONCURRENCY} simultanoues queries...`);
 
 results = async
   .map(results, async () => {
@@ -103,15 +107,16 @@ results = async
     });
   })
   .then(value => {
-    let passed = true;
+    let failed = 0;
     value.forEach(result => {
       console.log(result);
       if (!result.includes("Finished") || result.includes("300s")) {
-        passed = false;
+        failed += 1;
       }
     });
     end = performance.now();
     console.log(`\nTest completed in ${Math.ceil((end - start) / 1000)}s`);
-    console.log(`Test ${passed ? "passed!" : "failed."}`);
+    console.log(`Score: ${Math.round((CONCURRENCY - failed) / CONCURRENCY * 100)}%`)
+    console.log(`Test ${!failed ? "passed!" : "failed."}`);
     process.exit();
   });
