eb785ec5d1551f8afb432af96dd7c1d6a947176a
tokebe <43009413+tokebe@users.noreply.github.com>
2024-03-22 14:00:47 -0400

    feat: support for server typescript

--- scripts/smartapi_sync.js
+++ scripts/smartapi_sync.js
@@ -1,4 +1,4 @@
 process.env.DEBUG="bte*"
 process.env.SYNC_AND_EXIT="true" 
-sync = require("../packages/bte-server/built/controllers/cron/update_local_smartapi.js");
+const sync = require("../packages/bte-server/built/controllers/cron/update_local_smartapi.js").default;
 sync();
