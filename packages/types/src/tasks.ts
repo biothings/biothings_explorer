bce694bf3e062aff66fd353f5926663d49b78c43
tokebe <43009413+tokebe@users.noreply.github.com>
2024-03-22 14:06:08 -0400

    feat: add types package

---
+++ packages/types/src/tasks.ts
@@ -0,0 +1,75 @@
+import { APIList } from "./misc";
+import { TrapiQueryGraph, TrapiResponse, TrapiWorkflow } from "./trapi";
+
+// Options as they are assembled from the route
+export interface QueryOptions {
+  logLevel?: string;
+  submitter?: string;
+  smartAPIID?: string;
+  teamName?: string;
+  dryrun?: boolean;
+  caching?: boolean; // from request url query values
+}
+
+// Options as they are passed to the Query Handler
+export interface QueryHandlerOptions extends QueryOptions {
+  provenanceUsesServiceProvider?: boolean;
+  enableIDResolution?: boolean;
+  apiList?: APIList;
+  schema?: unknown; // might be hard to type -- it's the entire TRAPI schema IIRC
+  resolveOutputIDs?: boolean;
+  EDGE_ATTRIBUTES_USED_IN_RECORD_HASH?: string[];
+}
+
+export interface QueueData {
+  queryGraph: TrapiQueryGraph;
+  options: QueryOptions;
+  workflow?: TrapiWorkflow[];
+  callback_url?: string;
+  smartAPIID?: string;
+  teamName?: string;
+}
+
+export interface QueryParams {
+  id: string | number; // Job ID
+}
+
+export interface TaskData extends QueueData {
+  route: string;
+  params?: QueryParams;
+  endpoint?: string;
+  abortController?: AbortController;
+  url?: string;
+  enableIDResolution?: boolean;
+}
+
+// Info provided to outer task function (defined alongside route)
+export interface TaskInfo {
+  id?: string | number;
+  data: TaskData;
+}
+
+// Info provided to task handler (inside thread)
+export interface InnerTaskData {
+  req: TaskInfo;
+  route: string;
+  port: MessagePort;
+  job?: {
+    jobId: string | number;
+    queueName: string;
+  };
+}
+
+// Data sent from thread to main to keep track of execution
+// TODO break down into types of messages
+export interface DialHome {
+  threadId: string | number;
+  cacheInProgress?: number;
+  addCacheKey?: string;
+  completeCacheKey?: string;
+  registerId?: string;
+  cacheDone?: number;
+  err?: Error;
+  result?: TrapiResponse;
+  status?: number;
+}
---
+++ packages/types/src/trapi.ts
@@ -0,0 +1,186 @@
+export interface TrapiQNode {
+  ids?: string[];
+  categories?: string[];
+  is_set?: boolean;
+  constraints?: TrapiAttributeConstraint[];
+}
+
+export interface TrapiQEdge {
+  knowledge_type?: string;
+  predicates?: string[];
+  subject: string;
+  object: string;
+  attribute_constraints?: TrapiAttributeConstraint[];
+  qualifier_constraints?: TrapiQualifierConstraint[];
+}
+
+export interface TrapiQueryGraph {
+  nodes: {
+    [QNodeID: string]: TrapiQNode;
+  };
+  edges: {
+    [QEdgeID: string]: TrapiQEdge;
+  };
+}
+
+export interface TrapiSource {
+  resource_id: string;
+  resource_role: string;
+  upstream_resource_ids?: string[];
+}
+
+export interface TrapiKGNodes {
+  [nodeID: string]: TrapiKGNode;
+}
+
+export interface TrapiKGEdges {
+  [edgeID: string]: TrapiKGEdge;
+}
+
+export interface TrapiKnowledgeGraph {
+  nodes: TrapiKGNodes;
+  edges: TrapiKGEdges;
+}
+
+export interface TrapiKGEdge {
+  predicate: string;
+  subject: string;
+  object: string;
+  attributes?: TrapiAttribute[];
+  qualifiers?: TrapiQualifier[];
+  sources: TrapiSource[];
+}
+
+export interface TrapiKGNode {
+  categories: string[];
+  name: string;
+  attributes?: TrapiAttribute[];
+}
+
+export interface TrapiAttribute {
+  attribute_type_id: string;
+  original_attribute_name?: string;
+  value: string | string[] | number | number[];
+  value_type_id?: string;
+  attribute_source?: string | null;
+  value_url?: string | null;
+  attributes?: TrapiAttribute;
+  [additionalProperties: string]:
+  | string
+  | string[]
+  | null
+  | TrapiAttribute
+  | number
+  | number[];
+}
+
+export interface TrapiQualifier {
+  qualifier_type_id: string;
+  qualifier_value: string | string[];
+}
+
+export interface TrapiQualifierConstraint {
+  qualifier_set: TrapiQualifier[];
+}
+
+export interface TrapiAttributeConstraint {
+  id: string;
+  name: string;
+  not: boolean;
+  operator: string;
+  value: string | string[] | number | number[];
+}
+
+export interface TrapiNodeBinding {
+  id: string;
+  query_id?: string;
+  attributes?: TrapiAttribute[];
+}
+
+export interface TrapiEdgeBinding {
+  id: string;
+  attributes?: TrapiAttribute[];
+}
+
+export interface TrapiAnalysis {
+  resource_id?: string;
+  score?: number;
+  edge_bindings: {
+    [qEdgeID: string]: TrapiEdgeBinding[];
+  };
+  support_graphs?: string[];
+  scoring_method?: string;
+  attributes?: TrapiAttribute[];
+}
+
+export interface TrapiAuxiliaryGraph {
+  edges: string[];
+  attributes?: TrapiAttribute[];
+}
+
+export interface TrapiPfocrFigure {
+  figureUrl: string;
+  pmc: string;
+  matchedCuries: string[];
+  score: number;
+}
+
+export interface TrapiResult {
+  node_bindings: {
+    [qNodeID: string]: TrapiNodeBinding[];
+  };
+  analyses: TrapiAnalysis[];
+  pfocr?: TrapiPfocrFigure[];
+}
+
+export interface TrapiAuxGraphCollection {
+  [supportGraphID: string]: TrapiAuxiliaryGraph;
+}
+
+export interface TrapiLog {
+  timestamp: string;
+  level: string;
+  message: string;
+  code: string;
+}
+
+export interface TrapiWorkflow {
+  id: string;
+}
+
+export interface TrapiQueryMessage {
+  query_graph: TrapiQueryGraph;
+}
+
+export interface TrapiResponseMessage {
+  query_graph: TrapiQueryGraph;
+  knowledge_graph: TrapiKnowledgeGraph;
+  auxiliary_graphs?: TrapiAuxGraphCollection;
+  results: TrapiResult[];
+}
+
+export interface TrapiQuery {
+  message: TrapiQueryMessage;
+  log_level?: string;
+  workflow?: TrapiWorkflow[];
+  submitter?: string;
+  callback?: string;
+}
+
+export interface TrapiResponse {
+  status?: string;
+  description?: string;
+  schema_version?: string;
+  biolink_version?: string;
+  workflow?: TrapiWorkflow[];
+  message: TrapiResponseMessage;
+  logs: TrapiLog[];
+  trace?: string; // Only used in dev
+}
+
+export interface TrapiAsyncStatusResponse {
+  status: string;
+  description: string;
+  logs: TrapiLog[];
+  response_url?: string;
+}
